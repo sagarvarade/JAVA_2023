2c4e4b4203d4cbacbab1efd9e331b9352ea53226
APME-OFFICE\svarade <svarade@euronetworldwide.com>
2024-01-10 22:43:06 +0530

    fl

---
+++ SBApplication/src/main/java/com/SBApplication/controllers/TestCommunication.java
@@ -0,0 +1,29 @@
+package com.SBApplication.controllers;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("test")
+public class TestCommunication {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/names")
+	public String getNamesFromOtherApp() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		return restTemplate.exchange("http://localhost:8200/test/names", HttpMethod.GET, entity, String.class).getBody();
+	}
+}
